a8cf9878405a8ef066e782e48792e4f04ae7310c
rabbitCase <gnair3004@gmail.com>
2024-09-14 19:50:34 +0530

    Update and rename test.cpp to rotate.cpp

---
+++ Arrays and Hashing/rotate.cpp
@@ -0,0 +1,21 @@
+//Leetcode Medium 189: Rotate Array by k places//
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i,j,a[100],k,temp;
+    cin >> n;
+    for(i=0;i<n;i++)
+    cin >> a[i];
+    cin>>k;
+    for(i=0;i<k;i++){
+        temp=a[n-1];
+        for(j=n-1;j>=0;j--){
+            a[j]=a[j-1];
+        }
+        a[0]=temp;
+    }
+    cout << "\n";
+    for(i=0;i<n;i++){
+        cout << a[i] << " ";
+    }
+}
