86a0da6332105ff434cb412f8ca0fd597d6999ad
rabbitCase <gnair3004@gmail.com>
2024-09-15 19:35:03 +0530

    Update conatinsDuplicate.cpp

--- Arrays and Hashing/conatinsDuplicate.cpp
+++ Arrays and Hashing/conatinsDuplicate.cpp
@@ -1,3 +1,4 @@
+//Leetcode Easy 217: return true in given array has any duplicates
 #include <iostream>
 #include <bits/stdc++.h>
 #include <vector>
@@ -5,7 +6,7 @@ using namespace std;
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        vector<int> hash={0};
+        vector<int> hash={0};//initialize hash to 0
         sort(nums.begin(),nums.end());
         for(int i=1;i<nums.size();i++){
             if(nums[i]==nums[i-1]){
