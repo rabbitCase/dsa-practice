0b8f65948a480567024d6fbd5655d793a77b5010
rabbitCase <gnair3004@gmail.com>
2024-09-17 09:50:10 +0530

    Update Dll.c

--- Linked Lists/Dll.c
+++ Linked Lists/Dll.c
@@ -1,9 +1,55 @@
 //structure of a doubly linked list (updated with double pointers)
+#include <stdio.h>
+#include <stdlib.h>
+struct dnode{
+    struct dnode *llink;//left link
+    int data;
+    struct dnode *rlink;//right link
+}*head=NULL;
+
+void display(struct dnode *head){
+    if(head==NULL){
+        printf("\nList is empty.");
+        return;
+    }
+    struct dnode *cur=head;
+    printf("\nNew list: \n");
+    while(cur!=NULL){
+        printf("%d->",cur->data);
+        cur=cur->rlink;
+    }
+    printf("NULL\n");
+}
+
+void insertElement(struct dnode **head){//insert element at the rear end of list
+    int element;
+    printf("\nEnter data to be inserted: ");
+    scanf("%d",&element);
+    struct dnode *temp=(struct dnode*)malloc(sizeof(struct dnode));
+    temp->data=element;
+    temp->llink=NULL;
+    temp->rlink=NULL;
+    if(*head==NULL){
+        *head=temp;
+        return;
+    }
+    struct dnode *cur=*head;
+    while(cur->rlink!=NULL){
+        cur=cur->rlink;//get the last node
+    }
+    cur->rlink=temp;//connect temp to the list
+    temp->llink=cur;//doubly link the new node
+}
+
+int main() {
+    insertElement(&head);insertElement(&head);insertElement(&head);
+    display(head);
+    return 0;
+}
 
 
-
+//Previous code
 /*
-
 #include <stdio.h>
 #include <stdlib.h>
 struct dnode{
