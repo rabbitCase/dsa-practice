4635d4b0e9e1a8f8c61018660ebc32a8583f4cc7
rabbitCase <gnair3004@gmail.com>
2024-09-18 18:46:32 +0530

    Update buyAndSellStock.cpp

--- Arrays and Hashing/buyAndSellStock.cpp
+++ Arrays and Hashing/buyAndSellStock.cpp
@@ -1,4 +1,8 @@
 //Leetcode Easy 121: Best time to buy and sell stock
+/*Given an Array of prices, we have maximize our profit by buying and selling at the correct price
+    Concept: Suppose you buy a product for $2 and eventually, the price of that product goes upto $6,
+    now if you sell your product, you will recieve $6 which makes a total of $4 profit.
+    We have to implement the same concept for the array of prices over time of a particular stock*/
 #include <iostream>
 #include <bits/stdc++.h>
 #include <vector>
