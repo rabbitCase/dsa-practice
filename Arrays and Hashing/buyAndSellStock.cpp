165658acf4fff0c4248878e6f0a8aa9bf69faf2c
rabbitCase <gnair3004@gmail.com>
2024-09-18 18:39:48 +0530

    Add files via upload

---
+++ Arrays and Hashing/buyAndSellStock.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i,maxdiff=0,buy=prices[0];
+        for(i=1;i<prices.size();i++){
+        if(prices[i]<buy){
+            buy=prices[i];
+        }
+        else{
+        maxdiff=max(maxdiff,prices[i]-buy);
+        }
+        }
+        return maxdiff;
+    }
+};
