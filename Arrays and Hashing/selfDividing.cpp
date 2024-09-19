9f894854a57aa97daceed542e2f23e4b3e2649ab
rabbitCase <gnair3004@gmail.com>
2024-09-19 19:08:38 +0530

    Add files via upload

---
+++ Arrays and Hashing/selfDividing.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector <int> result;
+        int temp,i,num;
+        for(i=left;i<=right;i++){
+            bool check=true;
+            num=i;
+            while(num>0){
+              temp=num%10;
+              if(temp==0 || i%temp!=0){
+                check=false;
+              }
+              num/=10;
+              }
+            if(check){
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
+int main(){
+    Solution sol;
+    vector <int> x;
+    x=sol.selfDividingNumbers(1,2256654);
+    for(int i=0;i<x.size();i++){
+        cout<<x[i]<<" ";
+    }
+}
