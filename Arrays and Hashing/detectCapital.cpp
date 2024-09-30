1afc44ffd4dc1786fced9d58ebdb7537eb1d034d
rabbitCase <gnair3004@gmail.com>
2024-09-30 21:13:17 +0530

    Update detectCapital.cpp

--- Arrays and Hashing/detectCapital.cpp
+++ Arrays and Hashing/detectCapital.cpp
@@ -1,3 +1,5 @@
+//Leetcode Easy 520: Detect if the given string has used capitalization correctly
+//Definition of right usage of capitals: Only the first letter is capital or all letters are capital or all letters are lowercase
 #include <iostream>
 #include <cctype>
 using namespace std;
@@ -6,7 +8,7 @@ public:
     bool detectCapitalUse(string word) {
         bool capital;
         int n=word.length();
-        if(!isupper(word[0])){
+        if(!isupper(word[0])){//if first letter is lowercase then all further letters should be lowercase
                 capital=true;
             for(int i=1;i<n;i++){
                 if(isupper(word[i])){
@@ -15,7 +17,7 @@ public:
             }
         }
         else{
-            if(isupper(word[1])){
+            if(isupper(word[1])){//if the next letter is uppercase then all further letters should be uppercase
                 capital=true;
                 for(int i=2;i<n;i++){
                     if(!isupper(word[i])){
@@ -26,7 +28,7 @@ public:
             else{
                 capital=true;
                 for(int i=2;i<n;i++){
-                    if(isupper(word[i])){
+                    if(isupper(word[i])){//else if the next letter is lowercase then all further letters should be lowercase
                         capital=false;
                     }
                 }
