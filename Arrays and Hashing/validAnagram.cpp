c0f8a2f6cfb3663674dd91040515d913e44aff54
rabbitCase <gnair3004@gmail.com>
2024-10-04 21:54:42 +0530

    Update validAnagram.cpp

--- Arrays and Hashing/validAnagram.cpp
+++ Arrays and Hashing/validAnagram.cpp
@@ -1,3 +1,5 @@
+//Leetcode Easy 242: Return true if t is an anagram of s
+//Concept: An anagram is word which can be formed by reaaranging some other word, usually using every letter only once
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -6,10 +8,10 @@ public:
     bool isAnagram(string s, string t) {
         std::unordered_map<char,int> set;
         for(auto x:s){
-            set[x]++;
+            set[x]++;//push all characters of s into map
         }
         for(auto x:t){
-            set[x]--;
+            set[x]--;//if t is an anagram, after removing all letters of t from the map, the map should contain 0 characters
         }
         for(int i=0;i<set.size();i++){
             if(set[i]!=0)
