011b0751b75411c6561155692ef614bbee169813
rabbitCase <gnair3004@gmail.com>
2024-10-11 21:56:17 +0530

    Update rearrangeEle.cpp

--- Arrays and Hashing/Leetcode/Medium/rearrangeEle.cpp
+++ Arrays and Hashing/Leetcode/Medium/rearrangeEle.cpp
@@ -1,4 +1,9 @@
-//Leetcode Medium 2149
+//Leetcode Medium 2149: Reaarange Array
+//Problem Statement: Given an array consisting of equal number of positive and negative numbers we have to return a vector satisfying:
+//1) Vector should begin with a positive integer
+//2) Positive  and negative integer should alternate
+//3) The order of every interger with the same sign should be same as they were in the original vector
+/*Concept: Create two vectors, s1 and s2, one which stores the positive integers and another one for the negative integers, as order is preserved, simply append one element one positive integer and then one negative integer to a new vector s3 until s1 and s2 are empty*/
 #include <stdio.h>
 #include <bits/stdc++.h>
 using namespace std;
@@ -16,13 +21,14 @@ public:
             }
         }
         vector <int> s3;
-        for(int i=0;i<max(s1.size(),s2.size());i++){
+        for(int i=0;i<s1.size();i++){//size of s1 and s2 are same 
             s3.push_back(s1[i]);
             s3.push_back(s2[i]);
         }
         return s3;
     }
 };
+//Test
 int main(){
     Solution sol;
     vector <int> x={3,1,-2,-5,2,-4};
