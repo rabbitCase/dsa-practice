7976b911e40a66db3d47fa90408ad02e67d6a9ac
rabbitCase <gnair3004@gmail.com>
2024-10-11 22:00:05 +0530

    Update prefixSumQues.cpp

--- Arrays and Hashing/Leetcode/Medium/prefixSumQues.cpp
+++ Arrays and Hashing/Leetcode/Medium/prefixSumQues.cpp
@@ -1,8 +1,5 @@
 //Number of subarrays with sum K
-#include <iostream>
-#include <vector>
-#include <bits/stdc++.h>
-using namespace std;
+
 /* In a subarray with n elements prefix sum is iterating through 
 the array and storing the subsequent sum s. Now, to check the sum k 
 exists, we have to check if s-k exists in the hashmap we 
@@ -15,6 +12,10 @@ example: arr= 1,2,3,-3,1,1
          s-k=3,so we have to search for freq. of 3.
          s was 3 at points (1+2) and (1+2+3-3), so 
          hash[s-k]=hash[3]=2 therefore, count+=2 */
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
