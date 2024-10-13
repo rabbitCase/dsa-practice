0b4d2e4aadb2ffb8e4a85d1c672bd9a19ba4e40f
rabbitCase <gnair3004@gmail.com>
2024-10-13 11:01:50 +0530

    Update majEle.cpp

--- Arrays and Hashing/Leetcode/Easy/majEle.cpp
+++ Arrays and Hashing/Leetcode/Easy/majEle.cpp
@@ -1,6 +1,6 @@
 //Leetcode Easy 169 : Find the majority element in a given array. Majority element is the element which appears more than n/2 times where n is the size of the array.
 
-/*Concept: We can always use a hash map to store the frequency of every element in the array and then compare the frequency with n/2 to find the majority element, but there is a simple concept than can also be used: when we sort the array, the majority element should be present in the middle of the array, as the element occupies more than half the size of the array. So, we can simply sort our array (a vector in this case) and return the element at index n/2*/
+/*Concept: We can always use a hash map to store the frequency of every element in the array and then compare the frequency with n/2 to find the majority element, but there is a simple concept than can also be used: when we sort the array, the majority element must be present in the middle of the array, as the element occupies more than half the size of the array. So, we can simply sort our array (a vector in this case) and return the element at index n/2*/
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,7 +11,9 @@ public:
         return nums[(nums.size())/2];
     }
 };
-//Usgin unordered map (element mapped to its frequency)
+
+//We cannot use a simple array for hashing as we can be given negative integers, so the array will not be able to map the frequency of those numbers
+//Using unordered map (element mapped to its frequency)
 // class Solution {
 // public:
 //     int majorityElement(vector<int>& nums) {
@@ -30,6 +32,7 @@ public:
 //         return best;
 //     }
 // };
+
 //Test 
 int main(){
     Solution sol;
