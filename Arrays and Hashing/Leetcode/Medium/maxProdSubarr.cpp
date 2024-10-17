c49e8aca87a054df50fc4912160c3da698a15a88
rabbitCase <gnair3004@gmail.com>
2024-10-17 21:39:01 +0530

    Update maxProdSubarr.cpp

--- Arrays and Hashing/Leetcode/Medium/maxProdSubarr.cpp
+++ Arrays and Hashing/Leetcode/Medium/maxProdSubarr.cpp
@@ -1,4 +1,4 @@
-//Leetcode Medium 152: MAximum Product Subarray
+//Leetcode Medium 152: Maximum Product Subarray
 /*Concept: We have to find a subarray that has the largest product, and return the product. Here we can apply 2 loops, find every subarray and compare their products, but a better way to do this is apply the same concept we learned while calculating maximum subarray sum that is Kadane's algorithm. We modify the concept a little bit and use the algorithm for multiplication instead of addition. So we traverse through the array from both ends and store the subsequent product while comparing the maximum product obtained so far and store it in 'best'. At any point, if product becomes 0, then further product would just yield zero therefore in that case we set product to 1. The reason we traverse from both ends is that if we have a negative integer in the begining of the array then further products will all become negative, therefore it is necessary to traverse from the other end too.*/
 #include <iostream>
 #include <bits/stdc++.h>
