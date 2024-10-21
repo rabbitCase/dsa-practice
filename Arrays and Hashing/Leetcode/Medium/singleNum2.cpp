62d5e0febaa8f875767e617a051207cb017c80f6
rabbitCase <gnair3004@gmail.com>
2024-10-21 21:20:58 +0530

    Update singleNum2.cpp

--- Arrays and Hashing/Leetcode/Medium/singleNum2.cpp
+++ Arrays and Hashing/Leetcode/Medium/singleNum2.cpp
@@ -1,4 +1,5 @@
-//Leetcode Medium 137
+//Leetcode Medium 137: 2nd part of Easy 136 (Single Number). When using hash maps, the solution remains the same.
+/*The Question asks for linear time complexity and constant space. The common approach of using hash map (which is used here) solves the former but fails at the latter. The concept of bit manipulation is to be studied for fulfilling both the problem conditions. The problem may be updated in the future when I have a better grasp of bit manipulation*/
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,7 +12,7 @@ public:
         }
         for(auto x:mpp){
             if(x.second==1){
-            ans=x.second;
+            ans=x.first;
             break;
             }
         }
