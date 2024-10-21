46cb315d93975ab700d11097836db84ba7d1cfd9
rabbitCase <gnair3004@gmail.com>
2024-10-21 21:13:43 +0530

    Add files via upload

---
+++ Arrays and Hashing/Leetcode/Easy/singleNum.cpp
@@ -0,0 +1,20 @@
+//Leetcode Easy 136
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map <int,int>  mpp; int ans;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        for(auto x:mpp){
+            if(x.second==1){
+            ans=x.second;
+            break;
+            }
+        }
+        return ans;
+    }
+};
