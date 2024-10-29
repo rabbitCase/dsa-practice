fb00ee741175cc8270e534c648110f958aa13215
rabbitCase <gnair3004@gmail.com>
2024-10-29 10:02:29 +0530

    Update structTrees.c

--- Trees/College/structTrees.c
+++ Trees/College/structTrees.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node {
 
+    int data;
+
+    struct node* right;
+
+    struct node* left;
+} node;
+
+node* insert(node *root,int n){
+    node *temp=(node*)malloc(sizeof(node));
+    if(root==NULL){
+        temp->data=n;
+        temp->right=NULL;
+        temp->left=NULL;
+        return temp;
+    }
+    if(n < (root)->data){
+        root->left=insert(root->left,n);
+    }
+    else{
+        root->right=insert(root->right,n);
+    }
+    return root;
+}
+
+void traverse(node* root) {
+    if (root==NULL)
+        return;
+    traverse(root -> left);
+    printf("%d ", root -> data);
+    traverse(root -> right);
+}
+
+int main(){
+    node *root=NULL;
+    for(int i=0;i<10;i++){
+        root=insert(root,i);
+    }
+    traverse(root);
+
+    return 0;
+}
