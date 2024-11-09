dc5607d639af1c4bdd49105ae08efc58973e4a60
rabbitCase <gnair3004@gmail.com>
2024-11-09 18:45:50 +0530

    Add files via upload

---
+++ Arrays and Hashing/Leetcode/Easy/binarySearch.cpp
@@ -0,0 +1,31 @@
+//Leetcode Easy 704
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int high=nums.size()-1;
+        int low=0;
+        int mid;
+        while(low<=high){
+            mid=(high+low)/2;
+            if(nums[mid]<target)
+            low=mid+1;
+
+            else if(nums[mid]>target)
+            high=mid-1;
+
+            else 
+            return mid;
+
+        }
+        return -1;
+    }
+};
+
+int main(){
+    vector<int> num={-1,2,3,4,5};
+    Solution sol;
+    cout<< sol.search(num,4);
+}
