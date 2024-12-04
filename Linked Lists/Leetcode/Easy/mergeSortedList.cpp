bd7789e303ee1354048d679f25ca196ed2be140a
rabbitCase <gnair3004@gmail.com>
2024-12-04 18:52:08 +0530

    Update mergeSortedList.cpp

--- Linked Lists/Leetcode/Easy/mergeSortedList.cpp
+++ Linked Lists/Leetcode/Easy/mergeSortedList.cpp
@@ -1,16 +1,26 @@
-//Leetcode Easy 21
+//Leetcode Easy 21: Merge Sorted List
+//Given two sorted lists, return a sorted list merging the two.
+//Inefficient Solution: connect the tail of list1 to head of list2 and then bubble sort the new joined list
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 #include <iostream>
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1 && !list2)
+        return NULL;
+        else if(!list1 && list2)
+        return list2;
+        else if(!list2 && list1)
+        return list1;
         ListNode *cur=list1;
         int n1=1;
         bool isSwapped;
@@ -20,10 +30,6 @@ public:
         }
         ListNode *newcur=list2;
         cur->next=newcur;
-        while(newcur->next!=NULL){
-            n1++;
-            newcur=newcur->next;
-        }
         cur=list1; ListNode *newnext=cur->next;
         do{
             isSwapped=0;
@@ -41,16 +47,8 @@ public:
         }while(isSwapped);
         return list1;
     }
-
-    void display(ListNode *list1){
-        ListNode *cur=list1;
-        while(cur!=NULL){
-            std::cout<< cur->val<<"-> ";
-            cur=cur->next;
-        }
-        std::cout<<"NULL";
-    }
 };
+//Test main
 int main(){
     Solution sol;
     ListNode* list1 = new ListNode(1); list1->next = new ListNode(3); ListNode* list2 = new ListNode(4); list2->next = new ListNode(2);
