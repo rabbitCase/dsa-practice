ea767c7a42b4019f4800b2394979310101822f63
rabbitCase <gnair3004@gmail.com>
2024-12-07 12:31:04 +0530

    Update invertBinaryTree.cpp

--- Trees/Leetcode/Easy/invertBinaryTree.cpp
+++ Trees/Leetcode/Easy/invertBinaryTree.cpp
@@ -1,4 +1,6 @@
-//Leetcode Easy 226
+//Leetcode Easy 226: Invert a binary tree
+//Concept: We have to invert a binary tree i.e interchange nodes such that the resulting binary tree is the mirror image of the first. This can be done recursively by taking the root node and swapping it's left and right child and then call the function again but this time passing the swapped left and right node as parameters until all leaf nodes become null.
+//Recursive approach
 #include <iostream>
 struct TreeNode {
     int val;
