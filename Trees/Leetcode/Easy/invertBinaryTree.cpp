102bb1d97465585a30ec8e9cb14316c00c5aef67
rabbitCase <gnair3004@gmail.com>
2024-12-07 12:27:59 +0530

    Add files via upload

---
+++ Trees/Leetcode/Easy/invertBinaryTree.cpp
@@ -0,0 +1,25 @@
+//Leetcode Easy 226
+#include <iostream>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+ 
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root){
+            return root;
+        }
+        TreeNode *temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
