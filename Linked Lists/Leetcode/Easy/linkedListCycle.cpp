7c73f0c7555a2c8e40a38c1b2899886dc6270966
rabbitCase <gnair3004@gmail.com>
2024-12-08 19:37:16 +0530

    Update linkedListCycle.cpp

--- Linked Lists/Leetcode/Easy/linkedListCycle.cpp
+++ Linked Lists/Leetcode/Easy/linkedListCycle.cpp
@@ -1,4 +1,6 @@
-//Leetcode Easy 141
+//Leetcode Easy 141: return true if the given linked list has a cycle
+//Concept: create a hash map which pairs data of type ListNode (given linked list structure) to int. Traverse through the list, and increment every node's frequency by 1. If the list has a cycle, then it's frequency will be greater than 1. We can also use bool instead of int to mark the node as visited and flagging a cycle when a node set to 'visited' is visited again but this approach caused a TLE error. 
+//Slow and fast pointer approach is pending
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
