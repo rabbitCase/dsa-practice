af553fa5fb80ed6de403c1248a8ef25f9ecd765e
rabbitCase <gnair3004@gmail.com>
2024-12-09 17:22:37 +0530

    Update delNodeLl.cpp

--- Linked Lists/Leetcode/Medium/delNodeLl.cpp
+++ Linked Lists/Leetcode/Medium/delNodeLl.cpp
@@ -1,4 +1,4 @@
-//Leetcode Medium 237: Delete a given linked list node
+//Leetcode Medium 237: Delete a node in a given linked list
 //Condition: We do not have access to the head of the list. The function takes the node to be deleted as a parameter.
 //The question specifies to not actually delete the node from the memory but instead remove it's link such that a node with that value is no longer part of the original list.
 //Concept: As we don't have access to the head, we access the node to be deleted, change it's value to that of the succeeding node and then delete that succeeding the node. The question guarantees that the node to be deleted will not be the tail in any testcase so the above method works for all given testcases.
