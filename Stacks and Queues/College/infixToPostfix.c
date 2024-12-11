8ec603dcd0679bddf73783ad139380514117e0ad
rabbitCase <gnair3004@gmail.com>
2024-12-11 20:24:11 +0530

    Update infixToPostfix.c

--- Stacks and Queues/College/infixToPostfix.c
+++ Stacks and Queues/College/infixToPostfix.c
@@ -4,7 +4,7 @@
 3) If an operator is encountered, push it into the stack only if the precedence value of this operator is greater than the operator currently at the top of the stack.
 4) If the operator's precedence is not greater than the precedence of the element at the stack's top, then pop the elements (operators) of the stack until the precedence of the encountered element is greater than that of the operator at the top of the stack. All popped operators are simultaneously appended to the postfix expression as well.
 5) If an opening bracket '(' is encountered, we push it into the stack.
-6) If a closing bracket ')' is encountered, pop the elements of the stack until '(' is encountered and append the poppped elements into the postfix expression. Then pop one more time to remove the '(' as we dont need it.
+6) If a closing bracket ')' is encountered, pop the elements of the stack until '(' is encountered and append the poppped elements into the postfix expression. Then pop one more time to remove the '(' as it is not required.
 7) Finally pop all the remaining elements in the stack and append them to the postfix expression. Also add a null character '\0' at the end of the expression as it is a character array.
 */
 #include <stdio.h>
