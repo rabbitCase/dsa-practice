c80533a9cac00cbabe62f68c45e2eb3d482c494d
rabbitCase <gnair3004@gmail.com>
2024-12-15 21:53:30 +0530

    Update midLL.cpp

--- Linked Lists/Leetcode/Easy/midLL.cpp
+++ Linked Lists/Leetcode/Easy/midLL.cpp
@@ -11,31 +11,33 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
- 
-class Solution {
-public:
-    ListNode* middleNode(ListNode* head) {
-        int count=1;
-        if(!head){
-            return NULL;
-        }
-        ListNode *cur=head;
-        while(cur){
-            cur=cur->next;
-            count++;
-        }
-        cur=head;
-        if(count%2==0){
-            for(int i=0;i<count/2-1;i++){
+
+
+//Brute force
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         int count=1;
+//         if(!head){
+//             return NULL;
+//         }
+//         ListNode *cur=head;
+//         while(cur){
+//             cur=cur->next;
+//             count++;
+//         }
+//         cur=head;
+//         if(count%2==0){
+//             for(int i=0;i<count/2-1;i++){
                
-                cur=cur->next;
-            }
-            return cur;
-        }
-        for(int i=0;i<count/2-1;i++){
+//                 cur=cur->next;
+//             }
+//             return cur;
+//         }
+//         for(int i=0;i<count/2-1;i++){
                 
-                cur=cur->next;
-        }
-        return cur->next;
-    }
-};
+//                 cur=cur->next;
+//         }
+//         return cur->next;
+//     }
+// };
