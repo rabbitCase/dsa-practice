89c298e9bf550db671bc71e829eb521d9d6bfd3d
rabbitCase <gnair3004@gmail.com>
2024-12-15 21:54:15 +0530

    Update midLL.cpp

--- Linked Lists/Leetcode/Easy/midLL.cpp
+++ Linked Lists/Leetcode/Easy/midLL.cpp
@@ -1,5 +1,5 @@
 //Leetcode Easy 876: Find the middle node of a given linked list. Return the  second middle element if there are two of them (even no of elements)
-//There is always the brute force method where you traverse through the array, count it's size, traverse again till you reach the size/2 and return that element. But obviusly this is very inefficient and has vary bad space complexity.
+//There is always the brute force method where you traverse through the array, count it's size, traverse again till you reach the size/2 and return that element. But obviously this is very inefficient and has vary bad space complexity.
 //The concept of fast and slow pointers come to the rescue. Both fast and slow start at head, fast pointer moves two steps while the slow pointer moves one step, therefore, by the time the fast pointer reaches the end, the slow pointer will be at the middle of the list
 #include <iostream>
 using namespace std;
@@ -12,6 +12,21 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(!head){
+            return NULL;
+        }
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
 
 //Brute force
 // class Solution {
