cd2ddd9e753455b605badad7b44dd79fbcf86848
rabbitCase <gnair3004@gmail.com>
2024-12-20 21:51:04 +0530

    Update climbingStairs.cpp

--- Arrays and Math/Leetcode/Easy/climbingStairs.cpp
+++ Arrays and Math/Leetcode/Easy/climbingStairs.cpp
@@ -1,4 +1,5 @@
-//Leetcode Easy 70
+//Leetcode Easy 70: no of ways to climb n stairs
+//given an array of size n representing the no of stairs, return the no of ways to reach the top provided you can take either one or two steps at a time
 class Solution {
 public:
     int climbStairs(int n) {
