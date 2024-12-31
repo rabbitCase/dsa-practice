b0b7e15d671d3043879c71c549c3178c1184d993
rabbitCase <gnair3004@gmail.com>
2024-12-31 18:55:03 +0530

    Update ransomNote.cpp

--- Arrays and Math/Leetcode/Easy/ransomNote.cpp
+++ Arrays and Math/Leetcode/Easy/ransomNote.cpp
@@ -1,6 +1,6 @@
 //Leetcode Easy 383: Ransom Note
 //Given two strings ransomNote and magazine, determine wether ransomNote can be contructed using the characters in magazine, each magazine character can only be used once
-
+//Method 1
 #include <iostream>
 #include <bits/stdc++.h>
 class Solution {
@@ -22,3 +22,21 @@ public:
         return true;
     }
 };
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> magazinefreq; 
+        for (auto x : magazine) { 
+            magazinefreq[x]++; 
+        } 
+            
+        for (auto x : ransomNote) { 
+            if (magazinefreq[x] <= 0) { 
+                return false; 
+            }
+            magazinefreq[x]--; 
+        } 
+        return true;
+    }
+};
+
