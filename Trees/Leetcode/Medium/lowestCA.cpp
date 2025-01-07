9652b4429d5d98947909a819fc3d55abe05a2aff
rabbitCase <gnair3004@gmail.com>
2025-01-07 22:12:01 +0530

    Update lowestCA.cpp

--- Trees/Leetcode/Medium/lowestCA.cpp
+++ Trees/Leetcode/Medium/lowestCA.cpp
@@ -17,7 +17,7 @@ public:
             return NULL;
         }
         TreeNode *cur=root;
-        if(cur->val > p->val && cur->val > q->val)//we need to go left
+        if(cur->val > p->val && cur->val > q->val)//cur is towards the right of p and q so we need to go left
         return lowestCommonAncestor(cur->left,p,q);
         if(cur->val < p->val && cur->val < q->val)//we need to go right
         return lowestCommonAncestor(cur->right,p,q);
