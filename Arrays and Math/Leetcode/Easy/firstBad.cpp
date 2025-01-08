030e5583e3fbcef11683fb4b860b4e0ac3a267d6
rabbitCase <gnair3004@gmail.com>
2025-01-08 20:49:39 +0530

    Update firstBad.cpp

--- Arrays and Math/Leetcode/Easy/firstBad.cpp
+++ Arrays and Math/Leetcode/Easy/firstBad.cpp
@@ -1,6 +1,5 @@
 //Leetcode Easy 278: Find the first bad version
-//We have an array of size size n ([1,2,3...n]), where each number represents a version of a software. Each version after a particular version is 'bad'
-// The API isBadVersion is defined for you.
+//We have an array of size size n ([1,2,3...n]), where each number represents a version of a software. Each version after a particular version is 'bad'. To determine if a particular version is bad or not, we have been given an API isBadVersion which returns true if the version passed as a parameter is bad. We have to find the first bad version by making minimum no of calls to the API.
 // bool isBadVersion(int version);
 
 class Solution {
