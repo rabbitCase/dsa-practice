37c5aa03b673716c21609ecc58cd74e7e75cb4ae
rabbitCase <gnair3004@gmail.com>
2025-01-13 22:38:05 +0530

    Update 3sum.cpp

--- Arrays and Math/Leetcode/Medium/3sum.cpp
+++ Arrays and Math/Leetcode/Medium/3sum.cpp
@@ -22,10 +22,10 @@ public:
                 j++;
                 k--;
                 while(j<k && nums[j]==nums[j-1]){//for the current fixed index, we do not include the left pointer if the element it is pointing to was the same    
-                    j++;                         //after moving inward
+                    j++;                         //after moving towards the right in order to avoid duplicates
                 }
-                while(j<k && nums[k]==nums[k+1]){
-                    k--;
+                while(j<k && nums[k]==nums[k+1]){//for the current fixed index, we do not include the right pointer if the element it is pointing to was the same
+                    k--;                         //after moving towards the left in order to avoid duplicates
                 }
             }
             else if(sum<0){
@@ -39,3 +39,35 @@ public:
        return vec;
     }
 };
+//Method 2: In order to avoid the hassle of dealing with duplicates, we can use a set instead of a vector because it stores unique values only, however as the question asks us to return a vector, we have to copy the set to a vector and then retrn that vector. This can take a lot of time for large input data
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//        vector<vector<int>> vec;
+//        set<vector<int>> set;
+//        int size=nums.size();
+//        sort(nums.begin(),nums.end());
+//        for(int i=0;i<size;i++){
+//         int j=i+1;
+//         int k=size-1;
+//         while(j<k){
+//             int sum=nums[i]+nums[j]+nums[k];
+//             if(sum==0){
+//                 set.insert({nums[i],nums[j],nums[k]});
+//                 j++;
+//                 k--;
+//             }
+//             else if(sum<0){
+//                 j++;
+//             }
+//             else{
+//                 k--;
+//             }
+//         }
+//        }
+//        for(auto elements: set){
+//         vec.push_back(elements);
+//        }
+//        return vec;
+//     }
+// };
