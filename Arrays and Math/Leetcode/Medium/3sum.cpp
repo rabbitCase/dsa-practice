dc59657478545fd71cd73f83d69e614aca43729b
rabbitCase <gnair3004@gmail.com>
2025-01-13 22:31:35 +0530

    Update 3sum.cpp

--- Arrays and Math/Leetcode/Medium/3sum.cpp
+++ Arrays and Math/Leetcode/Medium/3sum.cpp
@@ -1,5 +1,6 @@
 //Leetcode Medium 15: 3 sum
 //find all triplets in an array which add upto 0
+//Concept: We first sort the array, then we can use the two pointer approach where we fix one index of the array and then initialise two pointers, one ahead of the fixed element and one at the end. If the elements at these 3 indices add upto 0, we push the triplet into the vector, if not, we move the pointers inward. We do this for every index. Note that moving the pointers inwards is working only because the array is sorted. It is also necesssary to avoid duplicates, so, we don't include an index to check if the element at that index was same previously.
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -10,7 +11,7 @@ public:
        int size=nums.size();
        sort(nums.begin(),nums.end());
        for(int i=0;i<size;i++){
-        if (i>0 && nums[i] == nums[i - 1]) 
+        if (i>0 && nums[i] == nums[i - 1]) //avoid checking for duplicates
         continue;
         int j=i+1;
         int k=size-1;
