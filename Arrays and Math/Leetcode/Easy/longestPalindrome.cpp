900eefef915a041e617c6c4a03e6618d2ce97bcb
rabbitCase <gnair3004@gmail.com>
2025-01-26 11:17:36 +0530

    Update longestPalindrome.cpp

--- Arrays and Math/Leetcode/Easy/longestPalindrome.cpp
+++ Arrays and Math/Leetcode/Easy/longestPalindrome.cpp
@@ -1,4 +1,5 @@
 //Leetcode Easy 409: find the length of the longest palindrome that can be formed with the given string
+//Concept: A palindrome can be formed if a char occurs with even frequency, for example: "aabbcc" can be used to form the palindrome: "abccba". There can only be one odd frequency character in our palindrome which will be placed in the middle (ex: "aabbccde" has "abcdcba" or "abcecba"as palindrome). Therefore, when we encounter an odd frequency char, we add it to the length only once, after that, on further odd frequency encounters, we only add the even part (as one odd character has already been added). 
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
