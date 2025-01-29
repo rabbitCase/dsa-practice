c3bba4522e8eea0a98e620e01468da011f849e72
rabbitCase <gnair3004@gmail.com>
2025-01-29 23:20:29 +0530

    Update kClosest.cpp

--- Mixed/kClosest.cpp
+++ Mixed/kClosest.cpp
@@ -1,6 +1,7 @@
 //Leetcode Medium 973: K closest points to origin
 //We have been given a vector matrix of type [[x0,y0],[x1,y1]...[xn,yn]] where [Xi,Yi] are coordinates in a 2D plane and an integer K. We have to find K number of points which are the closest to origin. In other words we have to find the euclidean distance of these points from (0,0) and return the K least distance.
 //Concept: Euclidean distance between two points is √((x1 - x2)2 + (y1 - y2)2), here, one the pair of coordinates are that of the origin so the distance simplies to √(x2 + y2). 
+//Incorrect approach: Using a map. A map could be used to store key-value pairs , mapping the distance with the coordinates. As a map stores unique values in sorted order, we could return the first k coordinates as they would already be sorted in ascending order (note that a map sortes by values and not keys, therefore it is important to map the distance to coordinates and not the coordinates to distance, as we need to return the first K sorted coordinates) 
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
