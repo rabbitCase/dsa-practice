06ff017a3e9340b5f434396899f189f6cad9ae1f
rabbitCase <gnair3004@gmail.com>
2025-01-29 23:14:24 +0530

    Update kClosest.cpp

--- Mixed/kClosest.cpp
+++ Mixed/kClosest.cpp
@@ -1,4 +1,6 @@
-//Leetcode Medium 973
+//Leetcode Medium 973: K closest points to origin
+//We have been given a vector matrix of type [[x0,y0],[x1,y1]...[xn,yn]] where [Xi,Yi] are coordinates in a 2D plane and an integer K. We have to find K number of points which are the closest to origin. In other words we have to find the euclidean distance of these points from (0,0) and return the K least distance.
+//Concept: Euclidean distance between two points is √((x1 - x2)2 + (y1 - y2)2), here, one the pair of coordinates are that of the origin so the distance simplies to √(x2 + y2). 
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
