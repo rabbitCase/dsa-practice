4cd5f726b781126a05fffbf762d9d5928e5bb32a
rabbitCase <gnair3004@gmail.com>
2025-02-08 11:54:46 +0530

    Update delMiddleLinkedList.cpp

--- Linked Lists/Leetcode/Medium/delMiddleLinkedList.cpp
+++ Linked Lists/Leetcode/Medium/delMiddleLinkedList.cpp
@@ -4,6 +4,8 @@
 
 //Method 2: We use the concept of fast and slow pointers we learned before when finding the middle of the linked list. The fast pointer moves twice as fast as the slow pointer, this way when the fast pointer reaches the end of the list, the slow pointer would reach the middle of the list. We keep track of the previous node as we update the slow pointer an then once the slow pointer reaches the middle, it can be deleted by changing the link of it's previous node. 
 
+//Modification: We don't we even need another pointer to keep track of the previous node if the fast pointer starts with head->next instead of head. This way, the slow pointer points to the node just before the middle node (the fast pointer null constraints must be modified to ensure this).
+
 //The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
 
 //Method 1
@@ -59,6 +61,24 @@ public:
             slow=slow->next;
         }
         prev->next=prev->next->next;// or prev->next=slow->next
+        return head;
+    }
+};
+//Modification without using prev
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(!head || !head->next){
+            return NULL;
+        }
+       
+        ListNode* fast=head->next;
+        ListNode* slow=head;
+        while(fast->next && fast->next->next){//if fast->next->next is not checked for NULL, the slow pointer will update to point towards the middle node in that iteration of the loop and the node after the middle node will be deleted. Take [1,3,4,7,1,2,6] for example, fast points to 3 then 7 then 2 at that time, slow points to 4, which is where it should be so that the middle node (7) could be deleted. Now, if fast->next->next is not cheked for null values, the loop will continue one more time where fast = fast->next->next becomes null. Now slow will point to 7, yielding wrong results.
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        slow->next=slow->next->next;
         return head;
     }
 };
