6596dac87a4c668ba10738b64e90406ae5e7eaf1
rabbitCase <gnair3004@gmail.com>
2025-06-30 18:00:22 +0530

    easy 844

---
+++ Arrays and Math/Leetcode/Easy/backStringComp.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        vector<char> vec1 ,vec2;
+        for(auto x : s){
+            vec1.push_back(x);
+            if(x == '#'){
+                vec1.pop_back();
+                if(!vec1.empty()){
+                    vec1.pop_back();
+                }
+                
+            }
+        }
+
+        for(auto x : t){
+            vec2.push_back(x);
+            if(x == '#'){
+                vec2.pop_back();
+                if(!vec2.empty()){
+                    vec2.pop_back();
+                }
+                
+            }
+        }
+        return vec1 == vec2;
+    }
+};
