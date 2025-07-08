90efa2f44f600ad86417f042baadac1499cec438
rabbitCase <gnair3004@gmail.com>
2025-07-08 20:52:33 +0530

    med 263

---
+++ Trees/Leetcode/Medium/lcaBT.cpp
@@ -0,0 +1,28 @@
+//Leetcode Medium 236
+#include <iostream>
+#include <bits/stdc++.h>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root == p || root == q){
+            return root;
+        }
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+        if(left == NULL){
+            return right;
+        }
+        if(right == NULL){
+            return left;
+        }
+        return root;
+    }
+};
