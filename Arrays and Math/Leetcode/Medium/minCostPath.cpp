ab3d516c606f1771b9e78f7ef1a17b383e296cff
rabbitCase <gnair3004@gmail.com>
2025-08-06 18:49:19 +0530

    med 64

---
+++ Arrays and Math/Leetcode/Medium/minCostPath.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        vector<vector<int>> memo(grid.size(), vector<int>(grid[0].size(), -1));
+        return traverse(grid, memo, 0, 0);
+    }
+    int traverse(vector<vector<int>>& grid, vector<vector<int>>& memo, int i, int j){
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if(i >= m || j >= n){
+            return INT_MAX;
+        }
+        if(i == m - 1 && j == n - 1){
+            return grid[i][j];
+        }
+        if(memo[i][j] != -1){
+            return memo[i][j];
+        }
+        return memo[i][j] = grid[i][j] + min(traverse(grid, memo, i, j + 1), traverse(grid, memo, i + 1, j));
+    }
+};
