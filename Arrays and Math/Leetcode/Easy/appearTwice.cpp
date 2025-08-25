b1ccaa40ae1f742dc9a17935055b1ed71f152fd8
rabbitCase <gnair3004@gmail.com>
2025-08-25 22:08:10 +0530

    easy 2351

--- Arrays and Math/Leetcode/Easy/appearTwice.cpp
+++ Arrays and Math/Leetcode/Easy/appearTwice.cpp
@@ -1,10 +1,12 @@
+//Leetcoed Easy 2351
+//current char is used as index in an array. The first index to reach value > 1 is the first letter to appear twice
 #include <iostream>
 #include <vector>
 using namespace std;
 class Solution {
 public:
     char repeatedCharacter(string s) {
-        vector<int> res(123, 0);
+        vector<int> res(26, 0);
         for(auto x : s){
             if(++res[x - 'a'] > 1){
                 return x;
