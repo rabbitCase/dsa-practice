052bce74be21205df74428954ae73af4b8f57877
rabbitCase <gnair3004@gmail.com>
2025-09-21 12:47:07 +0530

    med 78

---
+++ Arrays and Math/Leetcode/Medium/subsets.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> vec;
+        backtrack(nums, res, vec, 0);
+        return res;
+    }
+    void backtrack(vector<int>& nums, vector<vector<int>>& res, vector<int>& vec, int j){
+        res.push_back(vec);
+        for(int i = j; i < nums.size(); i++){
+            vec.push_back(nums[i]);
+            backtrack(nums, res, vec, i + 1);
+            vec.pop_back();
+        }
+    }
+};
